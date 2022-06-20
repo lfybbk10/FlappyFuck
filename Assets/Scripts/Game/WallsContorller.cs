f9c9d5fcf89a3bf255a3f557d68c823ec9640eda
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2022-06-21 01:57:50 +0300

    third

--- Assets/Scripts/Game/WallsContorller.cs
+++ Assets/Scripts/Game/WallsContorller.cs
@@ -6,9 +6,50 @@ using UnityEngine;
 public class WallsContorller : MonoBehaviour
 {
    [SerializeField] private GameObject wallsPrefab;
+   private GameObject walls;
+   private Rigidbody2D wallsRigidBody;
+   
+   private GameObject nextWalls;
+   private Rigidbody2D nextWallsRigidBody;
+   
+   private Settings settings;
+
+   private float speed;
+   private float speedChangeTimer = 0;
 
    private void Start()
    {
+      settings = GameObject.Find("Settings").GetComponent<Settings>();
+      walls = Instantiate(wallsPrefab, new Vector3(-3.4f, 0, 9), Quaternion.identity);
+      wallsRigidBody = walls.GetComponent<Rigidbody2D>();
       
+      nextWalls = Instantiate(wallsPrefab, new Vector3(-3.4f*-4, 0, 9), Quaternion.identity);
+      nextWallsRigidBody = nextWalls.GetComponent<Rigidbody2D>();
+
+      speed = settings.currentDifficult.speed;
+   }
+
+   private void Update()
+   {
+      speedChangeTimer += Time.deltaTime;
+      if (speedChangeTimer>=settings.currentDifficult.timeToChangeSpeed)
+      {
+         speedChangeTimer = 0;
+         speed += 1.5f;
+      }
+      
+      wallsRigidBody.velocity = Vector2.left * speed;
+      nextWallsRigidBody.velocity = Vector2.left * speed;
+
+      if (walls.transform.position.x <= -3.4f * 6)
+      {
+         Destroy(walls);
+         
+         walls = nextWalls;
+         wallsRigidBody = nextWallsRigidBody;
+         
+         nextWalls = Instantiate(wallsPrefab, new Vector3(-3.4f*-4, 0, 9), Quaternion.identity);
+         nextWallsRigidBody = nextWalls.GetComponent<Rigidbody2D>();
+      }
    }
 }
